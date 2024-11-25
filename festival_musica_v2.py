37d80773dc7895afc37693b0b2fc2c317386d3ad
Natalia <73670894+natfb@users.noreply.github.com>
2024-11-25 13:13:12 -0300

    .

--- festival_musica_v2.py
+++ festival_musica_v2.py
@@ -60,8 +60,8 @@ tables = {
             FOREIGN KEY(cod_festival) REFERENCES Festival (cod_festival)
             ) ENGINE=InnoDB """
     ),
-    'Alimentacao': (
-        """ CREATE TABLE Alimentacao (
+    'Alimentação': (
+        """ CREATE TABLE Alimentação (
             local varchar(200),
             horario time,
             nome varchar(100),
@@ -100,6 +100,9 @@ tables = {
         """ CREATE TABLE Festival_Participante (
             cod_participante int,
             cod_festival int,
+            avaliacao varchar(50),
+            genero_favorito varchar(50),
+            genero_festival varchar(50),
             FOREIGN KEY(cod_participante) REFERENCES Participante (cod_participante),
             FOREIGN KEY(cod_festival) REFERENCES Festival (cod_festival)
             ) ENGINE=InnoDB """
@@ -186,8 +189,22 @@ inserts = {
         (7, 'Lucas Rocha', '111.222.333-55', 'lucas@example.com'),
         (8, 'Rafael Lima', '222.333.444-66', 'rafael@example.com'),
         (9, 'Larissa Silva', '333.444.555-77', 'larissa@example.com'),
-        (10, 'Fernanda Sousa', '444.555.666-88', 'fernanda@example.com')
-    """),
+        (10, 'Fernanda Sousa', '444.555.666-88', 'fernanda@example.com'),
+        (11, 'Ricardo Araújo', '111.222.333-11', 'ricardo.araujo@example.com'),
+        (12, 'Carolina Mendes', '222.333.444-22', 'carolina.mendes@example.com'),
+        (13, 'Marcelo Barbosa', '333.444.555-33', 'marcelo.barbosa@example.com'),
+        (14, 'Isabela Rocha', '444.555.666-44', 'isabela.rocha@example.com'),
+        (15, 'Thiago Ribeiro', '555.666.777-55', 'thiago.ribeiro@example.com'),
+        (16, 'Renata Gomes', '666.777.888-66', 'renata.gomes@example.com'),
+        (17, 'Bruno Fernandes', '777.888.999-77', 'bruno.fernandes@example.com'),
+        (18, 'Patrícia Nunes', '888.999.111-88', 'patricia.nunes@example.com'),
+        (19, 'Gustavo Freitas', '999.111.222-99', 'gustavo.freitas@example.com'),
+        (20, 'Vanessa Correia', '111.222.333-00', 'vanessa.correia@example.com'),
+        (21, 'André Morais', '222.333.444-11', 'andre.morais@example.com'),
+        (22, 'Larissa Figueiredo', '333.444.555-22', 'larissa.figueiredo@example.com'),
+        (23, 'Rodrigo Silva', '444.555.666-33', 'rodrigo.silva@example.com')"""
+    ),
+    
     'Empresa': ("""INSERT INTO Empresa (cod_empresa, valor, cod_festival) 
         VALUES 
         (1, 20000.00, 1),
@@ -196,8 +213,8 @@ inserts = {
         (4, 22000.00, 4),
         (5, 25000.00, 5)"""
     ),
-    'Alimentacao': ("""
-        INSERT INTO Alimentacao (local, horario, nome, cod_empresa) 
+    'Alimentação': ("""
+        INSERT INTO Alimentação (local, horario, nome, cod_empresa) 
         VALUES 
         ('Praça de Alimentação', '12:00:00', 'Lanchonete A', 1),
         ('Área Gourmet', '18:00:00', 'Restaurante B', 2),
@@ -219,12 +236,67 @@ inserts = {
         (1, 9, 9),
         (2, 10, 10)"""
     ),
-    "Festival_Participante": ("""
-    INSERT INTO Festival_Participante (cod_participante, cod_festival) 
+    "Festival_Participante": ("""INSERT INTO Festival_Participante (cod_participante, cod_festival, avaliacao, genero_favorito, genero_festival) 
     VALUES 
-    (1, 1), (2, 1), (3, 1), (4, 2), (5, 2), (6, 2), (7, 3), (8, 3), (9, 4), (10, 5);
-    """
+    (1, 1, 'Gostou', 'Rock', 'Rock'), 
+    (1, 2, 'Nao Gostou', 'Rock', 'Pop'), 
+    (2, 1, 'Gostou', 'Rock', 'Rock'), 
+    (2, 3, 'Gostou', 'Samba', 'Samba'), 
+    (2, 4, 'Nao Gostou', 'Samba', 'Eletrônica'), 
+    (3, 1, 'Gostou', 'Rock', 'Rock'), 
+    (3, 2, 'Nao Gostou', 'Eletrônica', 'Pop'), 
+    (3, 4, 'Gostou', 'Eletrônica', 'Eletrônica'), 
+    (4, 2, 'Gostou', 'Pop', 'Pop'), 
+    (4, 3, 'Nao Gostou', 'Rock', 'Samba'), 
+    (4, 4, 'Gostou', 'Eletrônica', 'Eletrônica'), 
+    (5, 1, 'Nao Gostou', 'Samba', 'Rock'), 
+    (5, 3, 'Gostou', 'Samba', 'Samba'), 
+    (6, 2, 'Gostou', 'Pop', 'Pop'), 
+    (6, 4, 'Gostou', 'Eletrônica', 'Eletrônica'), 
+    (7, 1, 'Nao Gostou', 'Eletrônica', 'Rock'), 
+    (7, 2, 'Gostou', 'Pop', 'Pop'), 
+    (7, 3, 'Gostou', 'Samba', 'Samba'), 
+    (7, 4, 'Nao Gostou', 'Rock', 'Eletrônica'),
+	(8, 1, 'Gostou', 'Rock', 'Rock'), 
+    (8, 2, 'Nao Gostou', 'Rock', 'Pop'), 
+    (9, 1, 'Gostou', 'Rock', 'Rock'), 
+    (9, 3, 'Gostou', 'Samba', 'Samba'), 
+    (9, 4, 'Nao Gostou', 'Samba', 'Eletrônica'), 
+    (10, 1, 'Gostou', 'Rock', 'Rock'), 
+    (10, 2, 'Nao Gostou', 'Eletrônica', 'Pop'), 
+    (10, 4, 'Gostou', 'Eletrônica', 'Eletrônica'), 
+    (11, 2, 'Gostou', 'Pop', 'Pop'), 
+    (11, 3, 'Nao Gostou', 'Rock', 'Samba'), 
+    (11, 4, 'Gostou', 'Eletrônica', 'Eletrônica'), 
+    (12, 1, 'Nao Gostou', 'Samba', 'Rock'), 
+    (12, 3, 'Gostou', 'Samba', 'Samba'), 
+    (12, 2, 'Gostou', 'Pop', 'Pop'), 
+    (12, 4, 'Gostou', 'Eletrônica', 'Eletrônica'), 
+    (13, 1, 'Nao Gostou', 'Eletrônica', 'Rock'), 
+    (13, 2, 'Gostou', 'Pop', 'Pop'), 
+    (13, 3, 'Gostou', 'Samba', 'Samba'), 
+    (13, 4, 'Nao Gostou', 'Rock', 'Eletrônica'),
+	(14, 1, 'Gostou', 'Rock', 'Rock'), 
+    (14, 2, 'Nao Gostou', 'Rock', 'Pop'), 
+    (15, 1, 'Gostou', 'Rock', 'Rock'), 
+    (15, 3, 'Gostou', 'Samba', 'Samba'), 
+    (16, 4, 'Nao Gostou', 'Samba', 'Eletrônica'), 
+    (17, 1, 'Gostou', 'Rock', 'Rock'), 
+    (18, 2, 'Nao Gostou', 'Eletrônica', 'Pop'), 
+    (19, 4, 'Gostou', 'Eletrônica', 'Eletrônica'), 
+    (20, 2, 'Gostou', 'Pop', 'Pop'), 
+    (20, 3, 'Nao Gostou', 'Rock', 'Samba'), 
+    (20, 4, 'Gostou', 'Eletrônica', 'Eletrônica'), 
+    (21, 1, 'Nao Gostou', 'Samba', 'Rock'), 
+    (21, 3, 'Gostou', 'Samba', 'Samba'), 
+    (22, 2, 'Gostou', 'Pop', 'Pop'), 
+    (22, 4, 'Gostou', 'Eletrônica', 'Eletrônica'), 
+    (23, 1, 'Nao Gostou', 'Eletrônica', 'Rock'), 
+    (23, 2, 'Gostou', 'Pop', 'Pop'), 
+    (23, 3, 'Gostou', 'Samba', 'Samba'), 
+    (23, 4, 'Nao Gostou', 'Rock', 'Eletrônica')"""
     ),
+
     'Performance': ("""INSERT INTO Performance (hora_inicio, hora_fim, data, cod_performance, cod_artista, cod_palco) 
     VALUES 
     ('18:00:00', '20:00:00', '2024-11-25', 1, 1, 1),
@@ -253,12 +325,13 @@ inserts = {
 
 # lista com todos os nomes das tabelas 
 # ordem cosidera restricoes de chave primaria para futura exclusao
-table_names = ['Alimentacao', 'Funcionario', 'Seguranca', 'Empresa', 'Performance', 'Artista', 'Palco', 'Patrocinador', 'Venda_Ingresso', 'Ingresso', 'Venda', 'Festival_Participante', 'Participante', 'Festival']
+table_names = ['Alimentação', 'Funcionario', 'Seguranca', 'Empresa', 'Performance', 'Artista', 'Palco', 'Patrocinador', 'Venda_Ingresso', 'Ingresso', 'Venda', 'Festival_Participante', 'Participante', 'Festival']
 
 # dicionario preenchido iterando tables_names para criar codigos drop table
 drop = {}
 for name in table_names:
     drop[name] = (f"drop table {name}") 
+    
 
 # conexao com bando de dados local
 def connect_db():
@@ -347,6 +420,50 @@ def crud(connect):
     consulta3(connect)
 
 
+def update_test(connect): 
+    return 0
+
+def delete_test(connect):
+    return 0
+
+def consulta1(connect):
+    # ver quantos cada empresa ganhou com ingressos em todos os festivais. 
+    return 0
+
+def consulta2(connect):
+    # ver todos vestivais que um artista ja participou. n da grafico
+    # ver vestivais que irao ocorrer em tal data e tal cidade. facil
+    # ver quantos artistas um festival tem. x festival y n artistas. facil
+    # Quantidade de ingressos vendidos em cada data
+    # um participante ver quando ele comprou ingressos, data etc. facil
+    return 0
+
+def consulta3(connect):
+    # valor gasto por cada participante com ingressos
+    select_query = """SELECT 
+        p.nome AS Nome_Participante,
+        SUM(i.valor) AS Total_Gasto
+    FROM 
+        Participante p
+    JOIN 
+        Venda v ON p.cod_participante = v.cod_participante
+    JOIN 
+        Venda_Ingresso vi ON v.cod_venda = vi.cod_venda
+    JOIN 
+        Ingresso i ON vi.cod_ingresso = i.cod_ingresso
+    GROUP BY 
+        p.nome; """
+    cursor = connect.cursor()
+    cursor.execute(select_query)
+    myresult = cursor.fetchall()
+    for x in myresult:
+        print(x)
+    return 0
+
+
+def consulta_extra(connect):
+    return 0
+
 def show_table(connect):
     print("\n---SELECIONAR TABELA---")
     # Criação das tabelas
@@ -365,7 +482,6 @@ def show_table(connect):
         for x in myresult:
             print(x)
     cursor.close()
-
 
 def update_value(connect):
     print("\n---SELECIONAR TABELA PARA ATUALIZAÇÃO---")
@@ -394,55 +510,111 @@ def update_value(connect):
     connect.commit()
     cursor.close()
 
+
 def exit_db(connect):
     print("\n---EXIT DB---")
     connect.close()
     print("Conexão com o banco de dados foi encerrada!")
+    
 
-# falta implementar
-def update_test(connect): 
-    return 0
+def ia_model(connect):
+    
+    import pandas as pd
+    from sqlalchemy import create_engine
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.model_selection import train_test_split
+    
+    # Configurar a conexão com o banco de dados usando SQLAlchemy
+    conexao = create_engine("mysql+pymysql://root:admin@localhost/festival_musica")
 
-def delete_test(connect):
-    return 0
+    query = "SELECT * FROM festival_participante"
+    df = pd.read_sql(query, conexao)
 
-def consulta1(connect):
-    # ver quantos reais cada festival gerou em ingressos. 
-    return 0
+    df.to_csv('festival_participante.csv', index=False)
+    
+    #CRIANDO MODELO
+    ds = pd.read_csv("./festival_participante.csv")
+    ds.head()
 
-def consulta2(connect):
-    # ver todos festivais que um artista ja participou. n da grafico
-    # ver vestivais que irao ocorrer em tal data e tal cidade. facil
-    # ver quantos artistas um festival tem. x festival y n artistas. facil
-    #  Quantidade de ingressos vendidos em cada data
-    # um participante ver quando ele comprou ingressos. facil
-    return 0
+    #Entradas em X e saída em Y
+    y = ds.iloc[:, 2].values
 
-def consulta3(connect):
-    # valor gasto por cada participante com ingressos
-    select_query = """SELECT 
-        p.nome AS Nome_Participante,
-        SUM(i.valor) AS Total_Gasto
-    FROM 
-        Participante p
-    JOIN 
-        Venda v ON p.cod_participante = v.cod_participante
-    JOIN 
-        Venda_Ingresso vi ON v.cod_venda = vi.cod_venda
-    JOIN 
-        Ingresso i ON vi.cod_ingresso = i.cod_ingresso
-    GROUP BY 
-        p.nome; """
-    cursor = connect.cursor()
-    cursor.execute(select_query)
-    myresult = cursor.fetchall()
-    for x in myresult:
-        print(x)
-    return 0
+    ds.drop(columns=['cod_festival', 'avaliacao'], axis=1, inplace=True)
+    ds.head()
 
+    X = ds.iloc[:, 0:3].values
 
-# main
+    genero_favorito = LabelEncoder()
+    genero_festival = LabelEncoder()
+    avaliacao = LabelEncoder()
+
+    X[:, 1] = genero_favorito.fit_transform(X[:, 1])
+
+    X[:, 2] = genero_festival.fit_transform(X[:, 2])
+
+    y = avaliacao.fit_transform(y)
+
+    entrada = X[:-1]
+
+    saida = y[:-1]
+
+    #Dividindo a base em dados de teste e dados de treinamento
+    entrada_treino, entrada_teste, saida_treino, saida_teste = train_test_split(entrada, saida, random_state=0, train_size=0.75)
+
+    # Treinando o modelo RandomForestClassifier
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.metrics import accuracy_score
+
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(entrada_treino, saida_treino)
+
+    # Fazendo previsões
+    saida_pred = model.predict(entrada_teste)
+
+    # Avaliando o modelo
+    accuracy = accuracy_score(saida_teste, saida_pred)
+    print(f"Acurácia do modelo: {accuracy:.2f}")
+
+    saida_y = y[-1];
+    entrada_X = X[-1];
+
+    # Fazendo a previsão
+    avaliacao_prevista = model.predict(entrada_X.reshape(1, -1))
+    avaliacao_prevista
+
+    print(f"Cliente irá gostar do próximo festival?: {'Provavelmente Sim' if avaliacao_prevista[0] == 0 else 'Provavelmente Não'}")
+    
+    if entrada_X[2] == 0:
+        print('O gênero do festival é: Eletrônica')
+    elif entrada_X[2] == 1:
+        print('O gênero do festival é: Pop')
+    elif entrada_X[2] == 2:
+        print('O gênero do festival é: Rock')
+    elif entrada_X[2] == 3:
+        print('O gênero do festival é: Samba')
+    
+    if entrada_X[1] == 0:
+        print('O participante prefere música Eletrônica')
+    elif entrada_X[1] == 1:
+        print('O participante prefere música Pop')
+    elif entrada_X[1] == 2:
+        print('O participante prefere ouvir bandas de Rock')
+    elif entrada_X[1] == 3:
+        print('O participante prefere ouvir Samba')
+        
+
+def limpar_terminal():
+    
+    import os
+    # Limpa a tela dependendo do sistema operacional
+    # os.system('cls' if os.name == 'nt' else 'clear')
+    
+
+# Main
 try:
+    
+    limpar_terminal()
+    
     # Estabelece Conexão com o DB
     con = connect_db()
 
@@ -461,11 +633,12 @@ try:
         10. CONSULTA TABELAS INDIVIDUAIS
         11. UPDATE VALUES
         12. CLEAR ALL RESGATOCAO
+        13. IA
         0.  DISCONNECT DB\n """
         print(interface)
 
         choice = int(input("Opção: "))
-        if choice < 0 or choice > 12:
+        if choice < 0 or choice > 13:
             print("Erro tente novamente!")
             choice = int(input())
 
@@ -481,37 +654,53 @@ try:
             crud(con)
 
         if choice == 2:
+            limpar_terminal()
             create_all_tables(con)
 
         if choice == 3:
+            limpar_terminal()
             insert_test(con)
 
         if choice == 4:
+            limpar_terminal()
             update_test(con)
 
         if choice == 5:
+            limpar_terminal()
             delete_test(con)
 
         if choice == 6:
+            limpar_terminal()
             consulta1(con)
 
         if choice == 7:
+            limpar_terminal()
             consulta2(con)
 
         if choice == 8:
+            limpar_terminal()
             consulta3(con)
 
         if choice == 9:
-            print("n vai ter consulta extra")
+            limpar_terminal()
+            consulta_extra(con)
 
         if choice == 10:
+            limpar_terminal()
             show_table(con)
 
         if choice == 11:
+            limpar_terminal()
             update_value(con)
 
         if choice == 12:
+            limpar_terminal()
             drop_all_tables(con)
+            
+        if choice == 13:
+            limpar_terminal()
+            ia_model(con)
+            limpar_terminal()
 
     con.close()
 
